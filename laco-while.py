40067d90e6fb04aa4d592df6df5df2cf3240c3b5
sergiomerces <sergio.merces@gmail.com>
2026-01-28 23:48:40 -0300

    estrutura repetição for while

---
+++ laco-while.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um número: '))
+
+while numero != 0:
+    if numero % 2 == 0:
+        print('par')
+    else:
+        print('ímpar')
+
+    numero = int(input('Digite um número: '))
